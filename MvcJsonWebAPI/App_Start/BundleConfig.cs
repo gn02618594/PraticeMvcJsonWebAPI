159f1b8b5c7e0fb09e28c812efad461eb198e633
gn02618594 <gn02618594@gmail.com>
2021-01-31 17:44:07 +0800

    下一部份練習 Controller 傳遞 JSON 資料給 View 中 Chart.js 繪圖元件 所以先透過 Nuget 載入 Chart.js 然後在 BundleConfig 以及 Layout 設定 Chart.js

--- MvcJsonWebAPI/App_Start/BundleConfig.cs
+++ MvcJsonWebAPI/App_Start/BundleConfig.cs
@@ -25,6 +25,8 @@ namespace MvcJsonWebAPI
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/chart.js").Include("~/Scripts/Chart.min.js"));
         }
     }
 }
